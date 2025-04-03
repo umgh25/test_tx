c815054e7d94c2a4e5044783a4026eacb13b9c68
Usman <usman.mughal@outlook.fr>
2025-04-03 12:02:20 +0200

    refactor: Ajout de commentaire JSDOC

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,22 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+/**
+ * Métadonnées globales de la page, définissant le titre et la description pour l'optimisation SEO.
+ * @type {Metadata}
+ */
 export const metadata: Metadata = {
   title: "LiqTrade - Financement professionnel à court terme",
   description:
     "LiqTrade offre des solutions de financement professionnel à court terme, sans garantie personnelle. Prêt et affacturage pour les entreprises.",
 };
 
+/**
+ * Layout principal de l'application. Ce composant enveloppe l'ensemble du contenu de la page et applique la police Inter à tout le corps de la page.
+ *
+ * @param {React.ReactNode} children - Contenu de la page qui sera rendu à l'intérieur du layout.
+ * @returns {JSX.Element} - Structure HTML de la page, incluant le contenu passé dans `children`.
+ */
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="fr" dir="ltr">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,24 @@ import Hero from "@/components/layout/Hero";
 import { ServiceList } from "@/components/layout/ServiceList";
 import { FeatureGrid } from "@/components/layout/FeatureGrid";
 
+/**
+ * Composant principal de la page d'accueil.
+ *
+ * Ce composant sert de structure principale de la page d'accueil,
+ * affichant les différentes sections telles que l'en-tête, les services et les fonctionnalités.
+ *
+ * @returns {JSX.Element} Le rendu de la page d'accueil avec tous les éléments nécessaires.
+ */
 export default function Home() {
   return (
     <Layout>
+      {/* Affiche le composant Hero avec un message principal */}
       <Hero />
+
+      {/* Affiche la liste des services proposés par l'application */}
       <ServiceList />
+
+      {/* Affiche une grille de fonctionnalités proposées */}
       <FeatureGrid />
     </Layout>
   );
